bdf8af5dba8c39d4d03866c905a6e7e4da4eaa5b
Juan Pizarro <jpizarro@doingit.cl>
2011-02-14 02:51:40 -0300

    add thlib-server

---
+++ thlib-server-generic/src/main/java/com/jpizarro/th/server/generic/model/entity/Test.java
@@ -0,0 +1,25 @@
+package com.jpizarro.th.server.generic.model.entity;
+
+public class Test {
+	public static String test(){
+//		Hint hint = new Hint("Hint1","Hint esta en casa pe");
+//		
+//		Goal goal = new Goal("Goal1","Goal esta en casa pe");
+//		
+//		Competitor c = new Competitor("name1","desc1");
+//		c.setUserName("username1");
+//		c.setPassword("pass1");
+//		
+//		Team t = new Team("Team1");
+//		t.getCompetitors().add(c);
+//		
+//		Game g = new Game();
+//		
+//		return hint +"\n" +
+//			goal +"\n" +
+//			c +"\n" +
+//			t +"\n";
+		return "";
+	}
+
+}
